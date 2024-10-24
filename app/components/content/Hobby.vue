18d0cd2953961ab1c59e6275bba879dd622ee7f1
yjineey <did3706@naver.com>
2024-10-24 22:32:53 +0900

    hobby page 추가 , 컴포넌트 연결 부분 찾아서 수정해야함

---
+++ app/components/content/Hobby.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts"></script>
+
+<template>
+  <section class="mx-auto mt-4 flex max-w-4xl flex-col p-7 sm:mt-20">
+    <h1 class="font-newsreader italic text-white-shadow text-center text-4xl">
+      <ContentSlot :use="$slots.title" />
+    </h1>
+    <h2 class="text-center text-lg font-extralight italic text-muted">
+      <ContentSlot :use="$slots.subtitle" />
+    </h2>
+    <Divider class="mb-8 mt-2" />
+    <div class="flex flex-col gap-3 sm:flex-row sm:items-center sm:justify-between">
+      <HobbyProfilePicture />
+      <HobbyProfilePicture />
+      <HobbyProfilePicture />
+    </div>
+  </section>
+</template>
