4d311c67e85a0d29fe12d29fbb9ffc577b6274c8
yjineey <did3706@naver.com>
2024-10-29 21:31:02 +0900

    모바일에서도 사진 출력되도록 css 수정

--- app/components/hobby/Cooking2.vue
+++ app/components/hobby/Cooking2.vue
@@ -11,6 +11,13 @@
       <div class="relative">
         <NuxtImg
           width="256"
+          src="/assets/hobby/cooking2.PNG"
+          class="absolute inset-0 size-64 scale-110 rounded-xl object-cover blur-xl grayscale saturate-200 transition-all duration-300 group-hover:blur-[32px] group-hover:grayscale-0"
+          alt="aurora Profile Picture"
+          aria-label="aurora Profile Picture"
+        />
+        <NuxtImg
+          width="256"
           src="/assets/hobby/cooking2.jpg"
           class="size-64 rounded-xl object-cover transition-all duration-300"
           alt="aurora Profile Picture"
